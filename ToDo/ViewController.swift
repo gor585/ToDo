b1bd814d99d3a700b019c9cc67d1a1ca8c95b3ff
Yaroslav Stupinskyi <jaros.sv@gmail.com>
2018-03-30 10:42:52 +0300

    Initial Commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Jaroslav Stupinskyi on 30.03.18.
+//  Copyright © 2018 Jaroslav Stupinskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
